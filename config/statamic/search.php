d673a32b22c75f0ddf70370be90c3d6ffd30bede
unique-mechanic <128269030+unique-mechanic@users.noreply.github.com>
2025-07-30 15:36:45 +1000

    1. Restyled Tags page 2. Added partials in faq-tips, news page. 3. Added search functionality and used web.php to route them

--- config/statamic/search.php
+++ config/statamic/search.php
@@ -28,7 +28,7 @@ return [
         'default' => [
             'driver' => 'local',
             'searchables' => 'all',
-            'fields' => ['title'],
+            'fields' => ['title' , 'article_content'],
         ],
 
         // 'blog' => [
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::statamic('example', 'example-view', [
-//    'title' => 'Example'
-// ]);
+ Route::statamic('search', 'search');
